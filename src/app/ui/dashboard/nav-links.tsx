d5759308226f4f7fa9a01de741c091c0e2602e80
ingleshivam <ingleshivam@gmail.com>
2024-07-29 14:54:53 +0530

    NextJs

---
+++ src/app/ui/dashboard/nav-links.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Herr_Von_Muellerhoff } from 'next/font/google';
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+const links = [
+    {name:"Home",href:'/'},
+    {name:"Customer",href:'/dashboard/customer'},
+    {name:"Invoices",href:'/dashboard/invoices'}
+] 
+
+
+
+export default function NavLinks(){
+    const pathname = usePathname(); 
+    return(
+        <>
+            <ul className=''>
+                {
+                    links.map((link)=>{
+                        return (
+                            <Link key={link.name} href={link.href || ""}>
+                                <li className={clsx(
+                                    'p-4 text-center border border-white-500 text-white hover:bg-orange-500',
+                                    {
+                                        'bg-sky-500 text-white-600' : pathname == link.href
+                                    },
+                                    )}>{link.name}</li> 
+                            </Link>
+                        );      
+                    })
+                }
+            </ul>
+        </>
+    );
+}
---
+++ src/app/ui/dashboard/sidenav.tsx
@@ -0,0 +1,8 @@
+import NavLinks from "@/app/ui/dashboard/nav-links";
+const SideNav = () =>{
+    return (
+        <NavLinks></NavLinks>
+    );
+}
+
+export default SideNav;
